decd18374e03d3be7228f85e4f9f2acb691fb1fd
Generally2 <dailyspecial_719@yahoo.com>
2019-10-27 20:03:58 -0600

    Initial commit

---
+++ app/src/main/java/com/generally2/flyinggameapp/GameOverActivity.java
@@ -0,0 +1,37 @@
+package com.generally2.flyinggameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity {
+    private Button startGameAgain;
+    private TextView finalScore;
+    private String score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        score = getIntent().getExtras().get("score").toString();
+
+        startGameAgain = (Button) findViewById(R.id.play_again_btn);
+        finalScore = (TextView)findViewById(R.id.display_score);
+
+
+        startGameAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        finalScore.setText("Final Score: " + score);
+    }
+}
---
+++ app/src/main/java/com/generally2/flyinggameapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.generally2.flyinggameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FlyingFishLipsView gameView;
+    private Handler handler = new Handler();
+    private final static long Interval = 30;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        gameView = new FlyingFishLipsView(this);
+        setContentView(gameView);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0, Interval);
+    }
+}
