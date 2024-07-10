c9f61e200b6be6fb80bd76e29a1fe3dc7145892e
dailyspecial719 <dailyspecial_719@yahoo.com>
2024-07-10 08:39:08 -0600

    Initial commit

--- app/src/main/java/com/legion/flyinggameapp/SplashActivity.java
+++ app/src/main/java/com/legion/flyinggameapp/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run(){
                 try{
-                    sleep(5000);
+                    sleep(3000);
                 }
                 catch (Exception e){
                     e.printStackTrace();
